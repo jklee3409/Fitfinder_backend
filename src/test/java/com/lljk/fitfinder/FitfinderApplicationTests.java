39e8abfde4fa92feabaf9c24e0f25b4525f7bb64
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-26 22:18:24 +0900

    test(application): 초기 테스트 수정

---
+++ src/test/java/com/lljk/fitfinder/FitfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lljk.fitfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
