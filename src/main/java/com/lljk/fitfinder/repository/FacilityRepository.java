c208b234551e542a3ffc067eb823eac28e1d564c
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-26 23:15:36 +0900

    feat(repository): FacilityRepository 쿼리문

--- src/main/java/com/lljk/fitfinder/repository/FacilityRepository.java
+++ src/main/java/com/lljk/fitfinder/repository/FacilityRepository.java
@@ -1,14 +1,25 @@
 package com.lljk.fitfinder.repository;
 
 import com.lljk.fitfinder.entity.Facility;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
-    @Query("SELECT f FROM Facility f WHERE f.name = :name AND f.address = :address")
-    Optional<Facility> findByNameAndAddress(String name, String address);
+    @Query("SELECT f FROM Facility f WHERE " +
+            "f.latitude BETWEEN :latMin AND :latMax AND " +
+            "f.longitude BETWEEN :lonMin AND :lonMax AND " +
+            "f.type = :activity")
+    List<Facility> findFacilitiesByCriteria(
+            @Param("latMin") Double latMin,
+            @Param("latMax") Double latMax,
+            @Param("lonMin") Double lonMin,
+            @Param("lonMax") Double lonMax,
+            @Param("activity") String activity
+    );
 }
 
