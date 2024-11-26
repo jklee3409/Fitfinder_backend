18a10d1facd35e0a6ce38d2c5434eca2d49ee081
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-26 22:30:19 +0900

    feat(controller): UserController 구현
    사용자 등록 및 이메일 기반 검색 엔드포인트 API 구현

---
+++ src/main/java/com/lljk/fitfinder/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.lljk.fitfinder.controller;
+
+import com.lljk.fitfinder.entity.User;
+import com.lljk.fitfinder.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = userService.registerUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
