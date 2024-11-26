7ed9a1bd92f473bc4cb136989c2c668168969508
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-26 22:29:01 +0900

    feat(repository): UserRepository 구현
    JPARepository 상속

---
+++ src/main/java/com/lljk/fitfinder/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.lljk.fitfinder.repository;
+
+import com.lljk.fitfinder.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+}
